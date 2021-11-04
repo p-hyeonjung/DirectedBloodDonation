0def4ab224957d2a65c710178f40f01d95448c17
p-hyeonjung <phjung1811@gmail.com>
2021-11-04 23:14:08 +0900

    프로젝트 생성 및 기본설정

---
+++ src/main/java/com/web/dbd/HomeController.java
@@ -0,0 +1,54 @@
+package com.web.dbd;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home1() {
+	  ModelAndView mav = new ModelAndView();
+	  mav.setViewName("redirect:/index.jsp");
+    return mav;
+	}*/
+	
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+  public ModelAndView home2() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("redirect:/index.jsp"); // 파일 확장자 포함
+    
+    return mav;
+  }
+	
+	/*public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}*/
+	
+}
